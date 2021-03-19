c03b048d1c9425883bc0b0ead21688afa0530fc3
Kelsey Grover <kelsey.brahe.grover@gmail.com>
2021-03-19 14:35:42 -0600

    environ.prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyDVEYotS5sROqSv_EbgYUPahjsq524--1Y",
+    authDomain: "houseplant-encyclopedia.firebaseapp.com",
+    projectId: "houseplant-encyclopedia",
+    storageBucket: "houseplant-encyclopedia.appspot.com",
+    messagingSenderId: "972259775243",
+    appId: "1:972259775243:web:2aede4b44c48b0029fc80e"
+  }
 };
